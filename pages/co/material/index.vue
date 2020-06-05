5310cff31216b4b7e08d13f3c1ed06559170cf36
got.wong <wangzhaojian@zhibankeji.com>
2020-06-05 16:11:15 +0800

    -u files 1. 优化素材列表搜索

--- pages/co/material/index.vue
+++ pages/co/material/index.vue
@@ -2,20 +2,26 @@
 <v-container class="co-material">
     <v-card >
         <v-card-title>
-            素材管理
-            <v-spacer></v-spacer>
+            <v-col cols=12 md='3'>素材管理</v-col>
+            
+            <v-col cols=12 md='9'>
+            <v-form @submit.prevent="onSearch">
             <v-text-field
+                solo-inverted
+                flat
+                hide-details
                 v-model="search"
                 append-icon="search"
-                label="过滤本页内容"
+                label="输入关键字搜索素材"
                 single-line
-                hide-details
             ></v-text-field>
+            <button type='submit' hidden form='form'></button>
+            </v-form>
+            </v-col>
         </v-card-title>
         <v-card-text>
             <v-data-table
                 v-if='items && items.length > 0'
-                :search="search"
                 :headers="headers"
                 :items="items"
                 :options.sync="options"
@@ -33,19 +39,41 @@
                     <span>{{item.createSysUser ? item.createSysUser.name : item.createBy}}</span>
                 </template>
                 <template v-slot:item.shareCount="{ item }">
-                    <v-chip class='share-count-value' :color="getColor(item.shareCount)" dark>{{ item.shareCount }}</v-chip>
+                    <v-chip small class='share-count-value' :color="getColor(item.shareCount)" dark>{{ item.shareCount }}</v-chip>
                 </template>
                 <template v-slot:item.actions="{ item }">
                     <template v-if='item.loading'>
                         <v-progress-circular size='24' indeterminate color="primary"></v-progress-circular>
                     </template>
                     <template v-else>
-                        <nuxt-link :to='`/co/material/${item.id}`'><v-icon :small='!$vuetify.breakpoint.xsOnly' class="mx-1">edit</v-icon></nuxt-link>
-                        <v-icon :small='!$vuetify.breakpoint.xsOnly' @click="deleteItem(item)"  class="mx-1">delete</v-icon>
+                        <div style='min-width: 4rem;'>
+                        <nuxt-link :to='`/co/material/${item.id}`'><v-btn class="mx-1" fab :small='!$vuetify.breakpoint.xsOnly' color='info' icon><v-icon :small='!$vuetify.breakpoint.xsOnly'>edit</v-icon></v-btn></nuxt-link>
+                        <v-btn fab :small='!$vuetify.breakpoint.xsOnly' color='pink' @click.stop="deleteItem(item)" icon><v-icon color='pink' :small='!$vuetify.breakpoint.xsOnly' >delete</v-icon></v-btn>
+                        </div>
                     </template>
                 </template>
                 <template v-slot:item.status="{item}">
-                    <v-switch :disabled='item.loading' @change='onEnabled(item)' :true-value='1' :false-value='0' v-model="item.status" inset :label="`${!!item.status ? '启用' : '禁用'}`"></v-switch>
+                    <v-switch class='mr-n3' :disabled='item.loading' @change='onEnabled(item)' :true-value='1' :false-value='0' v-model="item.status" inset></v-switch>
+                    <!--  :label="`${!!item.status ? '启用' : '禁用'}`" -->
+                </template>
+                <template v-slot:expanded-item="{ headers, item }">
+                    <v-container no-gutters class='py-0'>
+                    <v-row no-gutters align='center'>
+                        <span class="grey--text mr-2">标签: </span>
+                        <v-chip
+                            v-for="(tag) in item.tags"
+                            :key="tag.value"
+                            class="ma-2"
+                            dense
+                            small
+                            color="teal"
+                            text-color="white"
+                            >
+                            {{ tag }}
+                            </v-chip>
+                        
+                    </v-row>
+                    </v-container>
                 </template>
             </v-data-table>
             <v-row align='stretch'><v-spacer></v-spacer><v-col style='flex-grow: 0'><v-pagination @input='onChangePagination' v-model="page" :length="paginationCount"></v-pagination></v-col></v-row>
@@ -87,14 +115,15 @@ export default {
             options: { page: 1, itemsPerPage: 10 },
             items: [],
             headers: [
+                { text: '状态', align: 'center', sortable: true, value: 'status' },
                 { text: '素材名', align: 'start', sortable: false, value: 'title' },
                 { text: '创建人', align: 'start', sortable: true, value: 'createBy' },
-                { text: '浏览量', align: 'start', sortable: true, value: 'browseCount' },
-                { text: '累计分享', align: 'start', sortable: true, value: 'shareCount' },
-                { text: '创建日期', align: 'start', sortable: true, value: 'createDate' },
-                { text: '修改日期', align: 'start', sortable: true, value: 'updateDate' },
-                { text: '状态', align: 'center', sortable: true, value: 'status' },
+                // { text: '浏览量', align: 'center', sortable: true, value: 'browseCount' },
+                { text: '创建日期', align: 'center', sortable: true, value: 'createDate' },
+                { text: '修改日期', align: 'center', sortable: true, value: 'updateDate' },
+                { text: '累计分享', align: 'center', sortable: true, value: 'shareCount' },
                 { text: '操作项', align: "center" ,value: 'actions', sortable: false },
+                // { text: '', value: 'data-table-expand' },
             ]
         }
     },
@@ -126,22 +155,38 @@ export default {
         }
     },
     methods: {
+        reset() {
+            this.page = this.options.page = 1
+            this.total = 0
+        },
+        onSearch() {
+            this.reset()
+            this.fetchList({ pageNo: this.options.page })
+        },
         onChangePagination(e) {
             this.fetchList({ pageNo: e })
         },
         fetchList(params) {
+            if (this.loading) {
+                return
+            }
             this.loading = true
-            this.$api.fetchAcrhiveList(assign({ pageSize: this.options.itemsPerPage }, params)).then(res => {
+            this.$nuxt.$loading.start()
+            this.$api.fetchAcrhiveList(assign({ pageSize: this.options.itemsPerPage, keywords: this.search }, params)).then(res => {
                 res = res.data
                 this.loading = false
                 this.total = res.total
                 this.items = res.content
+                this.$nuxt.$loading.finish()
                 history.pushState(
                     {},
                     null,
                     this.$route.path + '#' + encodeURIComponent(params ? params.pageNo : 1)
                 )
-            }, rej => this.loading = false)
+            }, rej => {
+                this.loading = false
+                this.$nuxt.$loading.finish()
+            })
         },
         getColor (count) {
             if (count > 400) return 'red'
